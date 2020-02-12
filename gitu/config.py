5f1e2d7fb341c1438dbd14f57e03f2df5cd526f9
Howyoung <howyoung3@gmail.com>
2020-02-12 16:01:29 +0800

    Refactor config module

---
+++ gitu/config.py
@@ -0,0 +1,158 @@
+import json
+import os
+
+import gitu
+
+CONF_ENV = 'GITU_CONF'
+CONF_FILE = 'config.json'
+LINUX_CONF_LOC = '/etc/gitu'
+NT_CONF_LOC = os.path.join(os.path.expanduser("~"), '.gitu')
+
+DEFAULT_CONFIG = {
+    'users': {
+    },
+    'aliases': {
+    }
+}
+
+GIT_CONF = {
+    'name': 'user.name',
+    'email': 'user.email',
+    'gpgkey': 'user.signingkey',
+    'gpgsign': 'commit.gpgsign'
+}
+
+
+class User:
+    def __init__(self):
+        self.name = None
+        self.email = None
+        self.gpgkey = None
+        self.gpgsign = None
+
+    @staticmethod
+    def from_dict(conf: dict):
+        user = User()
+        for k, v in user.__dict__.items():
+            setattr(user, k, conf.get(k))
+        return user
+
+    def get_git_configs(self) -> dict:
+        res = {}
+        for attr, git_conf in GIT_CONF.items():
+            conf = getattr(self, attr)
+            if conf is not None:
+                res[git_conf] = conf
+        return res
+
+    def to_dict(self):
+        return {k: v for k, v in self.__dict__.items() if v is not None}
+
+    def update(self, conf: dict):
+        for k, v in conf.items():
+            if v is not None:
+                setattr(self, k, v)
+
+
+def get_conf_file() -> str:
+    for conf_loc in (os.environ.get(CONF_ENV), os.curdir, NT_CONF_LOC, LINUX_CONF_LOC):
+        if conf_loc is None:
+            continue
+        conf_file = os.path.join(conf_loc, CONF_FILE)
+        if os.path.exists(conf_file):
+            return conf_file
+    return None
+
+
+def get_default_conf_loc() -> str:
+    loc_map = {
+        'posix': LINUX_CONF_LOC,
+        'nt': NT_CONF_LOC
+    }
+    return loc_map.get(os.name, os.curdir)
+
+
+class Config:
+    def __init__(self):
+        self.conf_file = get_conf_file()
+        if self.conf_file is None:
+            self.conf_file = os.path.join(get_default_conf_loc(), CONF_FILE)
+            self.users = {}
+            self.aliases = {}
+        else:
+            self.config = self._read_conf_file()
+
+    def _read_conf_file(self) -> dict:
+        if self.conf_file is None:
+            return None
+        with open(self.conf_file, 'r') as f:
+            conf = json.load(f, encoding='utf-8')
+            self.aliases = conf.get('aliases', {})
+            self.users = {}
+            for user_dict in conf.get('users', []):
+                user = User.from_dict(user_dict)
+                self.users[user.name] = user
+
+    def save(self) -> None:
+        os.makedirs(os.path.dirname(self.conf_file), exist_ok=True)
+        with open(self.conf_file, 'w')as f:
+            conf = {
+                'version': gitu.__version__,
+                'aliases': self.aliases,
+                'users': [user.to_dict() for user in self.users.values()],
+            }
+            json.dump(conf, f, indent=2)
+
+    def get_user(self, name_or_alias: str) -> User:
+        user = self.users.get(name_or_alias)
+        if user is None:
+            name_or_alias = self.aliases.get(name_or_alias)
+            return self.users.get(name_or_alias)
+        else:
+            return user
+
+    def add_user(self, user: User):
+        self.users[user.name] = user
+        self.save()
+
+    def get_users(self):
+        return self.users.values()
+
+    def remove_user(self, name):
+        if name not in self.users:
+            return
+        self.users.pop(name)
+        # Remove all aliases of the user
+        self.aliases = {alias: orig for alias, orig in self.aliases.items() if name != orig}
+        self.save()
+
+    def update_user(self, user):
+        self.users[user.name] = user
+        self.save()
+
+    def update_user_name(self, name, new_name):
+        user = self.users[name]
+        user.name = new_name
+        self.users[new_name] = user
+        self.users.pop(name)
+        aliases = self.aliases
+        for alias, orig in aliases.items():
+            if name == orig:
+                aliases[alias] = new_name
+        self.save()
+
+    def append_alias(self, name: str, alias: str):
+        self.aliases[alias] = name
+        self.save()
+
+    def remove_alias(self, alias: str):
+        if alias in self.aliases:
+            self.aliases.pop(alias)
+        self.save()
+
+    def get_aliases_of_users(self):
+        res = {}
+        for alias, name in self.aliases.items():
+            res.setdefault(name, [])
+            res[name].append(alias)
+        return res
--- gitu/functions.py
+++ gitu/functions.py
@@ -2,9 +2,9 @@ import subprocess
 import sys
 
 from gitu import git_config
-from gitu.config_manager import ConfigManager
+from gitu.config import Config, User
 
-config = ConfigManager()
+config = Config()
 
 USER_NOT_FOUND = 1
 USER_EXISTS = 2
@@ -26,7 +26,7 @@ def check_git():
         panic('Please make sure that your git has been installed and configured correctly.', GIT_ERROR)
 
 
-def get_user(name_or_alias) -> dict:
+def get_user(name_or_alias) -> User:
     user = config.get_user(name_or_alias)
     if user is None:
         panic('User %s not found.' % name_or_alias, USER_NOT_FOUND)
@@ -40,7 +40,7 @@ def get_cur_user(file: str = 'local') -> (str, str):
         email = git_config.get('user.email', file)
         return user, email
     except subprocess.CalledProcessError as ex:
-        if ex.stderr is not None:
+        if ex.stderr is not None and ex.stderr != '':
             print('git: ' + ex.stderr.rstrip('\n'), file=sys.stderr)
         panic('To get global user, use -g/--global.', GIT_ERROR)
 
@@ -49,7 +49,7 @@ def check_name_conflict(name_or_alias: str, replace: bool):
     user = config.get_user(name_or_alias)
     if user is not None:
         if replace:
-            config.remove_user(user['user.name'])
+            config.remove_user(user.name)
         else:
             panic('User %s already exists.' % name_or_alias, USER_EXISTS)
 
@@ -58,9 +58,9 @@ def login(args):
     check_git()
     user = get_user(args.name)
     try:
-        for k, v in user.items():
+        for k, v in user.get_git_configs().items():
             git_config.replace_all(k, str(v), args.git_conf_file)
-        print('You are now logged in as %s <%s>.' % (user['user.name'], user['user.email']))
+        print('You are now logged in as %s <%s>.' % (user.name, user.email))
     except subprocess.CalledProcessError as ex:
         if ex.stderr is not None:
             print('git: ' + ex.stderr.rstrip('\n'), file=sys.stderr)
@@ -70,15 +70,15 @@ def login(args):
 def register(args):
     check_name_conflict(args.name, args.replace)
     user = {
-        'user.name': args.name,
-        'user.email': args.email,
-        'commit.gpgsign': args.gpgsign,  # TODO: new user class will replace the bug when args are None here
-        'user.signingkey': args.gpgkey
+        'name': args.name,
+        'email': args.email,
+        'gpgsign': args.gpgsign,
+        'gpgkey': args.gpgkey
     }
-    config.add_user(user)
+    config.add_user(User.from_dict(user))
     if args.alias is not None:
         check_name_conflict(args.alias, args.replace)
-        config.append_alias(user['user.name'], args.alias)
+        config.append_alias(args.name, args.alias)
     print('%s <%s> registered.' % (args.name, args.email))
 
 
@@ -90,14 +90,14 @@ def view(args):
             aliases = config.get_aliases_of_users()
         users = config.get_users()
         for i, user in enumerate(users):
-            name = user['user.name']
-            print('%s <%s>' % (name, user['user.email']))
+            print('%s <%s>' % (user.name, user.email))
             if not args.all:
                 continue
-            alias = aliases.get(name, [])
+            alias = aliases.get(user.name, [])
             if len(alias) > 0:
-                print('%s: %s' % ('Alias' if len(alias) == 1 else 'Aliases', ', '.join(alias)))
-            print('GPG Sign: %s' % user.get('commit.gpgsign', False))
+                print('%s: %s' % ('alias' if len(alias) == 1 else 'aliases', ', '.join(alias)))
+            for k, v in user.to_dict().items():
+                print('%s: %s' % (k, v))
             if i != len(users) - 1:
                 print()
         if len(users) == 0:
@@ -108,27 +108,23 @@ def view(args):
 
 def remove(args):
     user = get_user(args.name)
-    config.remove_user(user['user.name'])
+    config.remove_user(user.name)
 
 
 def edit(args):
     user = get_user(args.name)
-    if args.email is not None:
-        user['user.email'] = args.email
-        config.update_user(user['user.name'], user)
+    conf = {
+        'email': args.email,
+        'gpgsign': args.gpgsign,
+        'gpgkey': args.gpgkey
+    }
+    user.update(conf)
+    config.update_user(user)
     if args.new_name is not None:
         check_name_conflict(args.new_name, args.replace)
-        config.update_user_name(user['user.name'], args.new_name)
+        config.update_user_name(user.name, args.new_name)
     if args.alias is not None:
         check_name_conflict(args.alias, args.replace)
-        config.append_alias(user['user.name'], args.alias)
+        config.append_alias(user.name, args.alias)
     if args.remove_alias is not None:
         config.remove_alias(args.remove_alias)
-    if args.gpgsign is not None:
-        user['commit.gpgsign'] = args.gpgsign
-        config.update_user(user['user.name'], user)
-    if args.gpgkey is not None:
-        user['user.signingkey'] = args.gpgkey
-        config.update_user(user['user.name'], user)
-
-    config.update_user(user['user.name'], user)
